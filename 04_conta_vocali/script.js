0c2f1f00ffeaba512e8c1da1fd14ec47e1057d60
Michele-Brignola <michele.brignola@hotmail.com>
2026-01-11 16:25:04 +0100

    added 04

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,18 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
-
+/**
+ * @param {string} word - Stringa della Parola
+ * @returns {number} - Numero di vocali
+ */
+function numeroVocali(word) {
+  const matches = word.match(/[aeiou]/gi)
+  return `${word} ha ${matches.length} vocali: ${matches}`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+alert(numeroVocali(word))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
