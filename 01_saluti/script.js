2d5c60ace0575c2724ae222795a22d40a7f17f9e
Michele-Brignola <michele.brignola@hotmail.com>
2026-01-11 15:39:14 +0100

    added 01

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,20 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
-
+const userName = "Mario";
 
 // Dichiara la funzione qui.
-
+/**
+ * @param {string} user
+ * @returns {string} Stringa con il saluto
+ **/
+function helloUser(user) {
+  return "Ciao " + user;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const saluto = helloUser(userName);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+
+alert(saluto);
---
+++ README.md
@@ -0,0 +1,13 @@
+# JS-SNACKS-FUNCTIONS
+
+### Consegna
+
+Risolvi gli snack che troverai in allegato definendo e invocando delle funzioni.
+🙅‍♂️ vietato usare map, filter, reduce!
+
+### BONUS
+
+Risolvere gli snack usando sia le funzioni tradizionali che le arrow functions.
+Risolvere il quarto snack nella cartellina 05 - bonus
+
+Buon divertimento
